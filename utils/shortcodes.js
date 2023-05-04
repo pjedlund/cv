cc2d7396a9f9030b311ca893a9772d76fee14bf3
Johan Edlund <me@johanedlund.se>
2023-05-04 17:33:19 +0000

    fancy ampersand css + shortcode added

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -19,5 +19,8 @@ module.exports = {
         return `<svg class="icon icon--${id}" role="img" aria-hidden="true" width="24" height="24">
                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-${id}"></use>
                 </svg>`
+    },
+    amp: function () {
+        return `<span class="amp">&amp;</span>`
     }
 }
