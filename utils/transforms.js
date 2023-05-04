8c41c79dc94b6f77904515e81b081e6338a616d0
Johan Edlund <me@johanedlund.se>
2023-05-04 14:16:18 +0000

    new prod build without critical css

--- utils/transforms.js
+++ utils/transforms.js
@@ -1,5 +1,4 @@
-const htmlmin = import('html-minifier')
-const critical = import('critical')
+const htmlmin = require('html-minifier')
 const buildDir = 'dist'
 
 const shouldTransformHTML = (outputPath) =>
@@ -10,33 +9,15 @@ const shouldTransformHTML = (outputPath) =>
 const isHomePage = (outputPath) => outputPath === `${buildDir}/index.html`
 
 process.setMaxListeners(Infinity)
+
 module.exports = {
     htmlmin: function (content, outputPath) {
         if (shouldTransformHTML(outputPath)) {
             return htmlmin.minify(content, {
-                useShortDoctype: true,
+                useShortDoctype: false,
                 removeComments: true,
                 collapseWhitespace: true
             })
-        }
-        return content
-    },
-
-    critical: async function (content, outputPath) {
-        if (shouldTransformHTML(outputPath) && isHomePage(outputPath)) {
-            try {
-                const config = {
-                    base: `${buildDir}/`,
-                    html: content,
-                    inline: true,
-                    width: 1280,
-                    height: 800
-                }
-                const { html } = await critical.generate(config)
-                return html
-            } catch (err) {
-                console.error(err)
-            }
         }
         return content
     }
